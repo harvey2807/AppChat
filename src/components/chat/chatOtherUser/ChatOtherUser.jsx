83532bea6c82fc60e93cf168c3c83a7764312b5b
Phung Khanh Duy <22130062@st.hcmuaf.edu.vn>
2025-12-20 21:51:08 +0700

    feat: render các file phương tiện của một khung chat. - render tin nhắn giữa 2 người.

--- src/components/chat/chatOtherUser/ChatOtherUser.jsx
+++ src/components/chat/chatOtherUser/ChatOtherUser.jsx
@@ -1,6 +1,13 @@
 import './ChatOtherUser.css'
 
+const userName = "duy"
+
 function ChatOtherUser() {
+    const messages = [{ "id": 11077, "name": "tttt", "type": 0, "to": "duy", "mes": "Bạn ơi tối nay đi chơi được không. Mọi chi phí tôi lo, bạn chỉ cần đi thôi.", "createAt": "2025-12-12 07:31:12" },
+    { "id": 11076, "name": "duy", "type": 0, "to": "tttt", "mes": "Ok luôn bạn ê.", "createAt": "2025-12-12 07:31:09" },
+    { "id": 11075, "name": "duy", "type": 0, "to": "tttt", "mes": "Mà cho tôi cái lịch đi bạn ơi.", "createAt": "2025-12-12 07:35:09" }]
+
+    
     return (
         <div className="chat-box">
             <div className="chat-box-header">
@@ -8,7 +15,9 @@ function ChatOtherUser() {
                 <text className='user-name'>Nguoi dung khac</text>
             </div>
             <div className="main-chat">
-
+                {messages.map(msg => (
+                    Message(msg)
+                ))}
             </div>
             <div className="chat-input">
                 <button className='image-btn' />
@@ -22,5 +31,33 @@ function ChatOtherUser() {
         </div>
     )
 }
+function Message(msg) {
+    if (msg.name === userName) {
+        return (
+            <div className="message me">
+                <div className="message-content">
+                    <p>{msg.mes}</p>
+                    <span className="time-send">
+                        {msg.createAt}
+                    </span>
+                </div>
+                <button className='avt' style={{ width: 15, height: 15 }} />
+            </div>
+        )
+    } else {
+        return (
+            <div className="message other">
+                <button className='avt' style={{ width: 15, height: 15 }} />
+                <div className="message-content">
+                    <p>{msg.mes}</p>
+                    <span className="time-send">
+                        {msg.createAt}
+                    </span>
+                </div>
+            </div>
+        )
+    }
+}
+// {"event":"GET_PEOPLE_CHAT_MES","status":"success","data":[{"id":11077,"name":"tttt","type":0,"to":"duy","mes":"ấd","createAt":"2025-12-12 07:31:12"},{"id":11076,"name":"duy","type":0,"to":"tttt","mes":"keeeeee","createAt":"2025-12-12 07:31:09"},{"id":11075,"name":"duy","type":0,"to":"tttt","mes":"klsdfds","createAt":"2025-12-12 07:31:02"},{"id":11074,"name":"tttt","type":0,"to":"duy","mes":"knn","createAt":"2025-12-12 07:30:59"},{"id":11073,"name":"tttt","type":0,"to":"duy","mes":"kjhjk","createAt":"2025-12-12 07:30:44"},{"id":11072,"name":"duy","type":0,"to":"tttt","mes":"sdfsf","createAt":"2025-12-12 07:30:34"},{"id":11071,"name":"tttt","type":0,"to":"duy","mes":"o;lsjdklf","createAt":"2025-12-12 07:30:26"},{"id":11069,"name":"duy","type":0,"to":"tttt","mes":"hello","createAt":"2025-12-12 07:27:22"},{"id":11068,"name":"duy","type":0,"to":"tttt","mes":"hello","createAt":"2025-12-12 07:26:55"}]}
 
 export default ChatOtherUser
--- src/components/chat/chatOtherUser/InfoChat.jsx
+++ src/components/chat/chatOtherUser/InfoChat.jsx
@@ -1,15 +1,77 @@
 import './InfoChat.css'
+import { useState } from 'react'
 
-function InfoChat(){
+function InfoChat() {
+    const [choseMedia, setChoseMedia] = useState(0)
+
+    const changeMedia = () => {
+        if (choseMedia === 0) setChoseMedia(1)
+        else setChoseMedia(0)
+        console.log(choseMedia)
+    }
+
+    const images = ["https://wp-cms-media.s3.ap-east-1.amazonaws.com/canh_dong_xanh_muot_tao_cam_giac_thu_thai_va_binh_yen_845456e2a6.jpg",
+        "https://nads.1cdn.vn/2024/11/22/74da3f39-759b-4f08-8850-4c8f2937e81a-1_mangeshdes.png",
+        "https://statictuoitre.mediacdn.vn/thumb_w/640/2017/7-1512755474943.jpg",
+        "https://img.tripi.vn/cdn-cgi/image/width=700,height=700/https://gcs.tripi.vn/public-tripi/tripi-feed/img/482812Fuv/anh-mo-ta.png",
+        "https://danangfantasticity.com/wp-content/uploads/2023/05/da-nang-trong-top-nhung-diem-den-co-phong-canh-nui-non-dep-nhat-chau-a.jpg"
+
+    ]
+    const files = ["filetest.txt",
+        "bikieptangai.pdf",
+        "cuu_am_chan_kinh.pdf"
+    ]
+
     return (
         <div className="infor">
-            <div className='inforOtherUser'>
-                
-            </div>
-            <div className='inforUser'>
+            <div className='inforOtherUserBox'>
+                <div className="inforOtherUser">
+                    <div className="avt-o"></div>
+                    <p>Tên người dùng</p>
+                </div>
+                <div className="media-box">
+                    <div className="btn-box">
+                        <button onClick={changeMedia}
+                            className='btn-media chose'
+                            style={{ backgroundColor: choseMedia === 0 ? 'cyan' : 'white' }}
+                        >Ảnh</button>
+                        <button onClick={changeMedia}
+                            className='btn-media'
+                            style={{ backgroundColor: choseMedia === 1 ? 'cyan' : 'white' }}
+                        >File</button>
+                    </div>
+                    <div className="media">
+                        <div className="image-box row gx-3"
+                            style={
+                                { visibility: choseMedia === 0 ? "visible" : "hidden" }
+                            }>
+                            {images.map((url, index) => (
+                                <div className="col-4 img">
+                                    <img key={index} src={url} alt={`img-${index}`} />
+                                </div>
+                            ))}
+                        </div>
+                        <div className="file-box"
+                            style={
+                                { visibility: choseMedia === 1 ? "visible" : "hidden" }
+                            }>
+                            {files.map((filename) => (
+                                <div className="file">
+                                    <div className="file-icon"></div>
+                                    <p className='file-name'>{filename}</p>
+                                    <button className="download-btn" />
+                                </div>
+                            ))}
 
+                        </div>
+                    </div>
+                </div>
             </div>
-
+            <div className='inforUserBox'>
+                <div className="avt avt-u"></div>
+                <p>Tên người dùng</p>
+                <button className='logout-btn'>Đăng xuất</button>
+            </div>
         </div>
     )
 }
