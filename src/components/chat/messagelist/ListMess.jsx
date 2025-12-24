33cb4e0915a15bdf64218bce6c739d6ee9cbe366
Đặng Trần Tấn Lực <dttanluc2004@gmail.com>
2025-12-24 18:56:56 +0700

    done theme for chat left

--- src/components/chat/messagelist/ListMess.jsx
+++ src/components/chat/messagelist/ListMess.jsx
@@ -1,19 +1,20 @@
 import './ListMess.css';
 
-function ListMess( onSelectChat) {
+function ListMess(onSelectChat) {
     return (
-        <div className="chat-list bg-white">
+        <div className="chat-list">
             <div className="list-group list-group-flush">
-                <a href="#" className="list-group-item list-group-item-action active-chat">
+                <a href="#" className="list-group-item list-group-item-action ">
                     <div className="d-flex w-100 align-items-center justify-content-between">
                         <div className="d-flex align-items-center">
-                            <div className="avatar me-3"></div> <div>
-                                <h6 className="mb-0 fw-bold">Tên người dùng khác</h6>
-                                <small className="text-muted">Bạn: tin nhắn mới nhất</small>
+                            <div className="avatar me-3"></div> 
+                            <div className="colorMessage">
+                                <h6 className="mb-0 ">Tên người dùng khác</h6>
+                                <small className="">Bạn: tin nhắn mới nhất</small>
                             </div>
                         </div>
                         <div className="d-flex flex-column align-items-end">
-                            <small className="text-muted mb-1">Giờ</small>
+                            <small className="colorMessage mb-1">Giờ</small>
                             <div className="status-dot unread"></div> </div>
                     </div>
                 </a>
@@ -22,13 +23,14 @@ function ListMess( onSelectChat) {
                     <div className="d-flex w-100 align-items-center justify-content-between">
                         <div className="d-flex align-items-center">
                             <div className="avatar me-3"></div>
-                            <div>
-                                <h6 className="mb-0 text-dark">Tên người dùng khác</h6>
-                                <small className="text-muted">tin nhắn mới nhất</small>
+                            <div className="colorMessage">
+
+                                <h6 className="mb-0 ">Tên người dùng khác</h6>
+                                <small className="">Bạn: tin nhắn mới nhất</small>
                             </div>
                         </div>
                         <div className="d-flex flex-column align-items-end">
-                            <small className="text-muted mb-1">Giờ</small>
+                            <small className="colorMessage mb-1">Giờ</small>
                             <div className="status-dot"></div>
                         </div>
                     </div>
